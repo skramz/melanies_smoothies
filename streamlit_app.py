21eff93a7a5e570fca42266b8af88f4d7a2d41fd
skramz <73723152+skramz@users.noreply.github.com>
2024-03-12 17:23:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,10 @@ if ingredients_list:
         time_to_insert = st.button('Submit Order')
 
 #   st.write(my_insert_stmt)
-    if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered!', icon="✅")
+if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+            
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
